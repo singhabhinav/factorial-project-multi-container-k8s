494c04c62ab6cb607441a2a4db2f6c2a7c2cffd4
Abhinav Singh <abhinav.foss@gmail.com>
2020-08-22 15:06:54 +0530

    Fixed express server api rules

--- express-server/index.js
+++ express-server/index.js
@@ -36,23 +36,23 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hi');
 });
 
-app.get('/api/values/all', async (req, res) => {
+app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
-app.get('/api/values/current', async (req, res) => {
+app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/api/values', async (req, res) => {
+app.post('/values', async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
